5c1885fcc98279927844dd50cf52c61d4ccd654a
sukala <sumitk31@gmail.com>
2022-02-17 16:31:27 +0530

    Debugs added

--- python-docker-sdk-examples/docker_sdk.py
+++ python-docker-sdk-examples/docker_sdk.py
@@ -1,11 +1,13 @@
 import docker
 client = docker.from_env()
-'''try:
-  streamer = client.images.pull("alpineno")
+try:
+  streamer = client.images.pull("alpine")
+  print("Image pulled")
 except docker.errors.ImageNotFound:
   print("Image not Found")
-  '''
+
 container = client.containers.run("alpine",["sh"], detach=True,stdin_open=True,tty=True)
+print("Image Running")
 container.exec_run("touch myfile.txt",stdout=True,stream=True);
 container.commit(repository="myalpine" ,tag="1.0")
 client.containers.run("myalpine:1.0",["sh"], detach=True,stdin_open=True,tty=True)
